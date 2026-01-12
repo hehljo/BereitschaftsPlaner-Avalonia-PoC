ea6f2d96bbc58cc2c507a4dc9f35b4ebbc5c5b3d
hehljo <hehljo@gmail.com>
2026-01-12 09:32:55 +0100

    Fix: Missing using directive for SBPUrlService

--- ViewModels/MainWindowViewModel.cs
+++ ViewModels/MainWindowViewModel.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using Avalonia.Media;
 using Avalonia.Platform.Storage;
 using BereitschaftsPlaner.Avalonia.Models;
+using BereitschaftsPlaner.Avalonia.Services;
 using BereitschaftsPlaner.Avalonia.Services.Data;
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
